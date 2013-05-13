a3b9704f4bd7c1bac2a0e13186447eda3fc8321b
Jonas Hjalmarsson <jonas.hjalmarsson@hultsfred.se>
2013-05-13 12:24:43 +0200

    ingress and image added, cron bugfixes

--- cbis-sync-options.php
+++ cbis-sync-options.php
@@ -13,6 +13,8 @@ function hk_cbis_options_add_page() {
 	add_options_page('HK CBIS Settings', 'CBIS Settings', 'manage_options', 'hk_cbis_options', 'hk_cbis_options_do_page') ;
 }
 
+
+
 // Draw the menu page itself
 function hk_cbis_options_do_page() {
 	global $wpdb;
@@ -24,23 +26,40 @@ function hk_cbis_options_do_page() {
 
 
 			<?php settings_fields('hk_cbis_options_options'); ?>
-			<?php $options = get_option('hk_cbis'); ?>
+			<?php $options = get_option('hk_cbis'); 
+			//echo "<br>update_categories  : " . $options["update_categories"];
+			//echo "<br>enable_cron  : " . $options["enable_cron"];
+			//echo "<br>update_products  : " . $options["update_products"];
+			?>
 
 			<?php submit_button(); ?>
 
 
 
-			<h3>DEBUG</h3>
-			<?php print_r( get_option('hk_cbis') ); ?>
+			<?php //echo "<h3>DEBUG</h3>";
+			//echo "<br>OPTIONS: <br>";
+			//print_r( $options ); ?>
 
 			
 			<h3>CBIS sync</h3>
 			<p><label for="hk_cbis[hk_cbis_key]">CBIS_API_KEY (i.e. JGFUTHL3N4MSCR7JRAH6PLZR2G3Y2XV1)</label><br/><input size="80" type="text" name="hk_cbis[hk_cbis_key]" value="<?php echo $options['hk_cbis_key']; ?>" /></p>
-			
+			<?php 
+			// CRON
+			if ( $options['hk_cbis_key'] != "" && $options['enable_cron'] == 1 && ! wp_next_scheduled( 'hk_cbis_hook' ) ) {
+				wp_schedule_event( time(), 'daily', 'hk_cbis_hook' );
+			}
+			else if ( ($options['hk_cbis_key'] == "" || $options['enable_cron'] != 1) && wp_next_scheduled( 'hk_cbis_hook' ) ) {
+				wp_clear_scheduled_hook( 'hk_cbis_hook' );
+			}
+			?>
 
 			<h3>Cron</h3>
 			<p><input type="checkbox" name="hk_cbis[enable_cron]" value="1"<?php checked( 1 == $options['enable_cron'] ); ?> /> <label for="hk_cbis[enable_cron]">Aktivera synkronisering.</label></p>
-			<p>Last run <?php echo Date("Y-m-d H:i:s",$options["hk_cbis_sync_time"]); ?>: <br><?php echo $options["hk_cbis_sync_log"]; ?></p>
+			<?php if (wp_next_scheduled( 'hk_cbis_hook' )) : ?>
+			<p>N&auml;sta synkronisering <?php echo Date("Y-m-d H:i:s",wp_next_scheduled( 'hk_cbis_hook' )); ?></p>
+			<?php else : ?>
+			<p>Ingen automatisk synkronisering.</p>
+			<?php endif; ?>
 
 			<h3>Rensa upp</h3>
 			<p><input type="checkbox" name="hk_cbis[cleanup]" value="1" /> <label for="hk_cbis[cleanup]">OBS! Detta tar bort allt som &auml;r synkat!</label></p>
@@ -65,7 +84,6 @@ function hk_cbis_options_do_page() {
 			<?php 
 			//JGFUTHL3N4MSCR7JRAH6PLZR2G3Y2XV1
 			if ( 0 == $options['cleanup'] ) {
-
 				define('CBIS_API_KEY', $options['hk_cbis_key']);
 
 
@@ -76,10 +94,7 @@ function hk_cbis_options_do_page() {
 				{
 					echo '<h3>CBIS -> WP (' . count($categories) . ' stycken)</h3>';
 					$categories = $example->getCategories(CbisExample::CBIS_LANGUAGE_SV);
-					foreach ($categories as $value) { ?>
-
-							<?php echo $value->Id . " " . $value->Name; ?>
-							<?php 
+					foreach ($categories as $value) {
 							$args = array(
 							'orderby'            => 'ID', 
 							'order'              => 'ASC',
@@ -94,9 +109,8 @@ function hk_cbis_options_do_page() {
 							'hide_if_empty'      => false,
 							'show_option_all' => 'Ingen' );  
 							wp_dropdown_categories($args);
-							echo '<br/>' ?>
-						
-					<?php 
+							echo $value->Id . " " . $value->Name;
+							echo '<br/>';
 					}
 				}
 				else { ?>
@@ -109,210 +123,28 @@ function hk_cbis_options_do_page() {
 				
 				
 				<h3>Produkter</h3>
-				<p><input type="checkbox" name="hk_cbis[update_products]" value="1" /> <label for="hk_cbis[update_products]">Uppdatera produkter fr&aring;n CBIS till WordPress.</label></p>
+				<p><input type="checkbox" name="hk_cbis[update_products]" value="1" /> <label for="hk_cbis[update_products]">Tvinga uppdatering produkter fr&aring;n CBIS till WordPress.</label></p>
 				<?php 
-
-				if ($options["update_products"])
+				
+				
+				if ($options["update_products"] == 1)
 				{
+					$log = "Tvingad uppdatering: \n" . hk_cbis_update_products(true) . "\n\nF&ouml;rra uppdateringen som lyckades:\n";
+				}
 
-					
-					$wp_posts = array();
-					// TODO!!!! SYNC ALL PRODUCTS AT ONCE, THEN DO THE CATEGORYMAPPING. SHOULD BE FASTER THEN QUERY MULTIPLE TIMES?!?!
-					//Search products
-					/* OLD
-					foreach($options['cbis_cat'] as $key => $value) {
-						if ($value > 0) {
-							$product_options = array(	
-														'languageId' => CbisExample::CBIS_LANGUAGE_SV,
-														'categoryId' => $key, 
-														'filter' => array(	'WithOccasionsOnly' => FALSE, 
-																			'ExcludeProductsNotInCurrentLanguage' => FALSE) 
-																			); 
-							$product_search = $example->getProducts($product_options);
-							echo 'Synka alla produkter fr&aring;n ' . $key . " till kategorien " . $value . ' i wordpress <br/>';
-							foreach ($product_search as $product) {
-								$id = $product->Id;
-								if (!array_key_exists($id, $wp_posts)) {
-									$name = $product->Name;
-									$content = "";
-									//print_r($product);
-									foreach ($product->Attributes->AttributeData as $data) {
-										//echo "data(" . $data->AttributeId . "): " . print_r($data->Value,true) . "<br>";
-										switch($data->AttributeId) {
-											case '102':
-												$content = $data->Value->Data;
-												break;
-										}
-										//Id
-										//Name
-										//Image
-										//Excerpt (101)
-										//Content (102)
-										//Status
-										//PublishDate
-										//ExpirationDate
-										//Address
-										//GPS (113 & 114)
-										//Contact
-
-									}
-									$wp_posts[$id] = array("name" => $name, "content" => $content, "category" => array($value));
-								}
-								else {
-									if (!in_array($value,$wp_posts[$id]["category"])) {
-										$wp_posts[$id]["category"][] = $value;
-									}
-								}
-								
-							}
-							//print_r($product_search);
-						}
-					} // end search products
-					*/
-
-					// get all products
-					$product_options = array(	
-												'languageId' => CbisExample::CBIS_LANGUAGE_SV,
-												'filter' => array(	'WithOccasionsOnly' => FALSE, 
-																	'ExcludeProductsNotInCurrentLanguage' => TRUE) 
-																	); 
-					$product_search = $example->getProducts($product_options);
-					echo 'Hittade '.count($product_search).' CBIS produkter, synkar till wordpress <br/>';
-					foreach ($product_search as $product) {
-						$id = $product->Id;
-						// just double check that not already added to wp_posts
-						if (!array_key_exists($id, $wp_posts)) {
-							$name = $product->Name;
-							$content = "";
-							
-							// get category or categories
-							$wp_cat = array();
-							if (!empty($product->Categories)) {								
-								if (is_object($product->Categories->Category)) {
-									$cat = $product->Categories->Category;
-									if (array_key_exists($cat->Id, $options["cbis_cat"]) && $options["cbis_cat"][$cat->Id] != 0) {
-										$wp_cat[] = $options["cbis_cat"][$cat->Id];
-									}
-								}
-								else {
-									foreach ($product->Categories->Category as $cat) {
-										if (array_key_exists($cat->Id, $options["cbis_cat"]) && $options["cbis_cat"][$cat->Id] != 0) {
-											$wp_cat[] = $options["cbis_cat"][$cat->Id];
-										}
-									}
-								}
-							}
-						
-							// get attributes and add to insert array if mapping category is found
-							if (!empty($wp_cat)) {
-								// get attributes
-								foreach ($product->Attributes->AttributeData as $data) {
-									//echo "data(" . $data->AttributeId . "): " . print_r($data->Value,true) . "<br>";
-									switch($data->AttributeId) {
-										case '102':
-											$content = $data->Value->Data;
-											break;
-									}
-									//Id
-									//Name
-									//Image
-									//Excerpt (101)
-									//Content (102)
-									//Status
-									//PublishDate
-									//ExpirationDate
-									//Address
-									//GPS (113 & 114)
-									//Contact
-
-								}
-								$wp_posts[$id] = array("name" => $name, "content" => $content, "category" => $wp_cat);
-							}
-						}
-						
-					}
-					
-					// end search products
-
-					// insert posts
-					$inserted = 0;
-					$updated = 0;
-					$unique = uniqid();
-					echo "L&auml;gg till eller uppdatera " . count($wp_posts) . "poster.<br/>";
-					foreach ($wp_posts as $id => $cbispost) {
-						$querystr = "
-							SELECT $wpdb->posts.* 
-							FROM $wpdb->posts, $wpdb->postmeta
-							WHERE $wpdb->posts.ID = $wpdb->postmeta.post_id 
-							AND $wpdb->postmeta.meta_key = 'cbis_product_id' 
-							AND $wpdb->postmeta.meta_value = '$id' 
-						";
-
-						$pageposts = $wpdb->get_results($querystr, OBJECT);
-
-						$newpost = array(
-							'comment_status' => 'closed', // 'closed' means no comments.
-							//'post_author'    => <user ID>, //TODO, pick which author to assign post
-							'post_content'   => $cbispost["content"], //The full text of the post.
-							'post_status'    => 'publish', //Set the status of the new post.
-							'post_title'     => $cbispost["name"], //The title of your post.
-							'post_type'      => 'post', //You may want to insert a regular post, page, link, a menu item or some custom post type
-						); 
-						// if post exist, do update
-						if ($pageposts) {
-							$newpost['ID'] = $pageposts[0]->ID;  //Are you updating an existing post?
-							$updated++;
-						}
-						else {
-							$inserted++;
-						}
-
-						// insert or update post
-						$wp_id = wp_insert_post($newpost, true);
-
-						if (!is_wp_error($wp_id)) {				
-							// set categories
-							wp_set_post_terms($wp_id, $cbispost["category"], "category");
-
-							// set custom fields
-							add_post_meta($wp_id, 'cbis_product_id', $id, true) or update_post_meta($wp_id, 'cbis_product_id', $id);
-							add_post_meta($wp_id, 'cbis_product_unique', $unique, true) or update_post_meta($wp_id, 'cbis_product_unique', $unique);
-						}
-						else
-							echo "is_error?";
-						echo "$inserted nya poster och $updated uppdaterade.<br/>";
-						flush();
-					} // end foreach insert posts
-					echo "Nu har det lagts till eller uppdaterats poster.<br/>";
-					
-					echo "$inserted nya poster och $updated uppdaterade.<br/>";
-					flush();
-					
-					// clean up old
-					$querystr = "
-						SELECT $wpdb->posts.* 
-						FROM $wpdb->posts, $wpdb->postmeta
-						WHERE $wpdb->posts.ID = $wpdb->postmeta.post_id 
-						AND $wpdb->postmeta.meta_key = 'cbis_product_unique'  
-						AND $wpdb->postmeta.meta_value != '$unique'  
-					";
-
-					$pageposts = $wpdb->get_results($querystr, OBJECT);
-					$count = 0;
-					foreach($pageposts as $post) {
-						$count++;
-						delete_post_meta($post->ID,"cbis_product_id");
-						wp_delete_post($post->ID,true);
-					}
-					echo "Nu &auml;r $count gamla poster som inte inneh&ouml;ll $unique borta.<br>";
-
-				} // end if update_products
-			
 			// what to do with the rest?? How do we remove? Check all with custom_field cbis_product_id and compare...
 			
 			} // end if not checked cleanup
 			
+			
+			// LOG
+			
 			?>
+			<br><br>LOG: <br>
+			<p><textarea name="hk_cbis[log]" cols=80 rows=15 ><?php echo $log; echo $options["log"];  ?></textarea></p>
+			
+			
+			
 			
 
 
@@ -321,6 +153,193 @@ function hk_cbis_options_do_page() {
 		</form>
 	</div>
 	<?php
+}
+
+add_action( 'hk_cbis_hook', 'hk_cbis_update_products' );
+function hk_cbis_update_products($returnlog = false) {
+
+	global $wpdb;
+	$options = get_option('hk_cbis');
+	define('CBIS_API_KEY', $options['hk_cbis_key']);
+
+	$example = new CbisExample();
+	$wp_posts = array();
+	
+	$log = "Synkade senast " . Date("Y-m-d H:i:s") . "\n";
+
+	// get all products
+	$product_options = array(	
+								'languageId' => CbisExample::CBIS_LANGUAGE_SV,
+								'filter' => array(	'WithOccasionsOnly' => FALSE, 
+													'ExcludeProductsNotInCurrentLanguage' => TRUE) 
+													); 
+	//print_r($product_options);
+	$product_search = $example->getProducts($product_options);
+	
+	$log .= "Hittade " . count($product_search) . " CBIS produkter, synkar till wordpress \n";
+	foreach ($product_search as $product) {
+		$id = $product->Id;
+		// just double check that not already added to wp_posts
+		if (!array_key_exists($id, $wp_posts)) {
+			$name = $product->Name;
+			$content = "";
+			$ingress = "";
+			
+			// get category or categories
+			$wp_cat = array();
+			if (!empty($product->Categories)) {								
+				//print_r($product->Categories->Category);
+				if (is_object($product->Categories->Category)) {
+					$cat = $product->Categories->Category;
+					if (array_key_exists($cat->Id, $options["cbis_cat"]) && $options["cbis_cat"][$cat->Id] != 0) {
+						$wp_cat[] = $options["cbis_cat"][$cat->Id];
+					}
+				}
+				else {
+					foreach ($product->Categories->Category as $cat) {
+						if (array_key_exists($cat->Id, $options["cbis_cat"]) && $options["cbis_cat"][$cat->Id] != 0) {
+							$wp_cat[] = $options["cbis_cat"][$cat->Id];
+						}
+					}
+				}
+			}
+		
+			// get attributes and add to insert array if mapping category is found
+			if (!empty($wp_cat)) {
+				// get attributes
+				
+				foreach ($product->Attributes->AttributeData as $data) {
+					//echo "data(" . $data->AttributeId . "): " . print_r($data->Value,true) . "<br>";
+					switch($data->AttributeId) {
+						case '101': //ingress
+							$ingress = $data->Value->Data;
+							break;
+						case '102': //content
+							$content = $data->Value->Data;
+							break;
+						case '115': //image
+							if (count($data->Value->MediaList->MediaObject) > 1) {
+								$imageurl = $data->Value->MediaList->MediaObject[0]->Url;
+								$imagedescr = $data->Value->MediaList->MediaObject[0]->Description;
+							}
+							elseif (count($data->Value->MediaList->MediaObject) == 1) {
+								$imageurl = $data->Value->MediaList->MediaObject->Url;
+								$imagedescr = $data->Value->MediaList->MediaObject->Description;
+							}
+								
+							break;
+					}
+					//Id
+					//Name
+					//Image
+					//Excerpt (101)
+					//Content (102)
+					//Status
+					//PublishDate
+					//ExpirationDate
+					//Address
+					//GPS (113 & 114)
+					//Contact
+
+				}
+				if ($ingress != "")
+					$content = "<p class='ingress'>" . $ingress . "</p>" . $content . "";
+				
+				if ($imageurl != "") 
+					$content = "<img src='$imageurl&width=540&height=315&fitaspect=1' alt='$description'/>" . $content;
+					
+				$wp_posts[$id] = array("name" => $name, "content" => $content, "category" => $wp_cat);
+			}
+		}
+		
+	}
+	
+	// end search products
+
+	// insert posts
+	$inserted = 0;
+	$updated = 0;
+	$unique = uniqid();
+	$log .=  "L&auml;gg till eller uppdatera " . count($wp_posts) . " poster.\n";
+	foreach ($wp_posts as $id => $cbispost) {
+		$querystr = "
+			SELECT $wpdb->posts.* 
+			FROM $wpdb->posts, $wpdb->postmeta
+			WHERE $wpdb->posts.ID = $wpdb->postmeta.post_id 
+			AND $wpdb->postmeta.meta_key = 'cbis_product_id' 
+			AND $wpdb->postmeta.meta_value = '$id' 
+		";
+
+		$pageposts = $wpdb->get_results($querystr, OBJECT);
+
+		$newpost = array(
+			'comment_status' => 'closed', // 'closed' means no comments.
+			//'post_author'    => <user ID>, //TODO, pick which author to assign post
+			'post_content'   => $cbispost["content"], //The full text of the post.
+			'post_status'    => 'publish', //Set the status of the new post.
+			'post_title'     => $cbispost["name"], //The title of your post.
+			'post_type'      => 'post', //You may want to insert a regular post, page, link, a menu item or some custom post type
+		); 
+		// if post exist, do update
+		if ($pageposts) {
+			$newpost['ID'] = $pageposts[0]->ID;  //Are you updating an existing post?
+			$updated++;
+		}
+		else {
+			$inserted++;
+		}
+
+		// insert or update post
+		$wp_id = wp_insert_post($newpost, true);
+
+		if (!is_wp_error($wp_id)) {				
+			// set categories
+			wp_set_post_terms($wp_id, $cbispost["category"], "category");
+
+			// set custom fields
+			add_post_meta($wp_id, 'cbis_product_id', $id, true) or update_post_meta($wp_id, 'cbis_product_id', $id);
+			add_post_meta($wp_id, 'cbis_product_unique', $unique, true) or update_post_meta($wp_id, 'cbis_product_unique', $unique);
+		}
+		else
+			$log .=  "is_error?";
+		$log .=  "$inserted nya poster och $updated uppdaterade. (id: " . $wp_id . ")\n";
+
+	} // end foreach insert posts
+	$log .=  "Nu har det lagts till eller uppdaterats poster.\n";
+	
+	$log .=  "$inserted nya poster och $updated uppdaterade.\n";
+
+	
+	// clean up old
+	$querystr = "
+		SELECT $wpdb->posts.* 
+		FROM $wpdb->posts, $wpdb->postmeta
+		WHERE $wpdb->posts.ID = $wpdb->postmeta.post_id 
+		AND $wpdb->postmeta.meta_key = 'cbis_product_unique'  
+		AND $wpdb->postmeta.meta_value != '$unique'  
+		AND $wpdb->postmeta.meta_value != ''
+	";
+
+	$pageposts = $wpdb->get_results($querystr, OBJECT);
+	$count = 0;
+	foreach($pageposts as $post) {
+		$count++;
+		delete_post_meta($post->ID,"cbis_product_id");
+		wp_delete_post($post->ID,true);
+	}
+	$log .=  "Nu &auml;r $count gamla poster som inte inneh&ouml;ll $unique borta.\n";
+	
+	$log .= "Avslutade synkning " . Date("Y-m-d H:i:s") . "\n";
+
+	$options = get_option('hk_cbis');
+	$options["log"] = $log;
+	$options["update_products"] = 0;
+	update_option('hk_cbis', $options);
+	if ($returnlog) 
+		return $log;
+	else
+		return;
+
 }
 
 // Sanitize and validate input. Accepts an array, return a sanitized array.
