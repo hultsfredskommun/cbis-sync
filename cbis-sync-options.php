6de9a8e7cf6453ee69162e5eca89ec138028e3e6
Jonas Hjalmarsson <jonas.hjalmarsson@hultsfred.se>
2013-05-20 09:30:36 +0200

    bug

--- cbis-sync-options.php
+++ cbis-sync-options.php
@@ -219,7 +219,7 @@ function hk_cbis_update_products($returnlog = false) {
 					}
 				}
 				else {
-					if (!empty($product->Categories->Category) : foreach ($product->Categories->Category as $cat) {
+					if (!empty($product->Categories->Category)) : foreach ($product->Categories->Category as $cat) {
 						if (array_key_exists($cat->Id, $options["cbis_cat"]) && $options["cbis_cat"][$cat->Id] != 0) {
 							$wp_cat[] = $options["cbis_cat"][$cat->Id];
 						}
